eaad97d289a5ec946022b48d451b29d77a253b3e
Kant <98311953+Kladex@users.noreply.github.com>
2022-08-02 12:07:32 +0700

    Create ex-6.js

---
+++ ex-6.js
@@ -0,0 +1,11 @@
+const carCollection = ["toyota", "fiat", "honda", "bmw"];
+function updateCarCollection(carBrand) {
+	// Start coding here
+}
+
+//ผลลัพธ์ที่ควรได้จาก Example case
+//ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
+console.log(updateCarCollection("audi")); //new car collection is : toyota,fiat,honda,bmw,audi.
+
+//ในกรณีที่มียี่ห้อรถใน carCollection
+console.log(updateCarCollection("toyota")); //toyota has already existed in the 1 position of car collection.
