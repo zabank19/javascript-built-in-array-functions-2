576672aa6059e290365e06b4e43e1af64d65938d
Kant <98311953+Kladex@users.noreply.github.com>
2022-08-02 11:12:43 +0700

    Create ex-5.js

---
+++ ex-5.js
@@ -0,0 +1,12 @@
+let crimeRecordNames = ["Theodora Milenko", "Tinashe Benigno", "Mahalia Priyanka", "Maisy Sudhir"];
+
+function searchCrimeRecord(name) {
+	// Start coding here
+}
+
+//ผลลัพธ์ที่ควรได้จาก Example case
+//ในกรณีที่ไม่เคยมีประวัติเกี่ยวกับอาชญากรรม
+console.log(searchCrimeRecord("Dilan Sandra")); //"Dilan Sandra has no crime record"
+
+//ในกรณีที่เคยมีประวัติเกี่ยวกับอาชญากรรม
+console.log(searchCrimeRecord("Tinashe Benigno")); //"Tinashe Benigno has crime record !!!"
