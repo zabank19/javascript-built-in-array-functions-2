aea08883083f15562f8946b4404268864a2f83b6
Kant <98311953+Kladex@users.noreply.github.com>
2022-08-02 12:40:12 +0700

    Create ex-7.js

---
+++ ex-7.js
@@ -0,0 +1,8 @@
+function isPalindrome(string) {
+	// Start coding here
+}
+
+//Example case
+console.log(isPalindrome("reviver"));// true
+console.log(isPalindrome("บวบ"));// true
+console.log(isPalindrome("deliver"));// false
